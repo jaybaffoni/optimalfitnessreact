06beb22366e0ea42f6e423cdf3df17b1fa2c3ff3
Jay Baffoni <jay@baffoni5.com>
2021-07-12 19:07:11 -0400

    close menu on mobile click

--- src/components/header-bar.jsx
+++ src/components/header-bar.jsx
@@ -4,6 +4,7 @@ import {EnvelopeOpen, Facebook, Instagram} from "react-bootstrap-icons";
 import logo from '../assets/images/optimal-white-logo-only.png';
 import {NavLink} from "react-router-dom";
 import {config} from "../config";
+import {useState} from "react";
 
 const styles = {
     logo: {
@@ -22,26 +23,29 @@ const styles = {
 }
 
 export function HeaderBar(props) {
+
+    const [expanded, setExpanded] = useState(false);
+
     return (
-        <Navbar expand="lg" variant="dark" sticky="top" style={{background: config.primaryColor, borderBottom: 'solid 1px white'}}>
+        <Navbar expand="lg" variant="dark" sticky="top" style={{background: config.primaryColor, borderBottom: 'solid 1px white'}} expanded={expanded} onToggle={setExpanded}>
             <div className="container">
                 <img src={logo} style={styles.logo}/>
                 <Navbar.Brand href="/home" style={{marginTop: 2}}>Optimal Fitness</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
-                        <NavLink to="/home" style={styles.link} activeStyle={styles.activeLink}>Home</NavLink>
-                        <NavLink to="/about" style={styles.link} activeStyle={styles.activeLink}>About Us</NavLink>
+                        <NavLink onClick={() => setExpanded(false)} to="/home" style={styles.link} activeStyle={styles.activeLink}>Home</NavLink>
+                        <NavLink onClick={() => setExpanded(false)} to="/about" style={styles.link} activeStyle={styles.activeLink}>About Us</NavLink>
                         <NavDropdown style={{...styles.link, marginTop: 4}} title="Locations" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/lexington">Lexington Performance Center</NavDropdown.Item>
-                            <NavDropdown.Item href="/beachhouse">Natick Beach House</NavDropdown.Item>
-                            <NavDropdown.Item href="/corporate">Corporate</NavDropdown.Item>
-                            <NavDropdown.Item href="/quin">The 'Quin</NavDropdown.Item>
-                            <NavDropdown.Item href="/inhome">In Home</NavDropdown.Item>
+                            <NavDropdown.Item onClick={() => setExpanded(false)} href="/lexington">Lexington Performance Center</NavDropdown.Item>
+                            <NavDropdown.Item onClick={() => setExpanded(false)} href="/beachhouse">Natick Beach House</NavDropdown.Item>
+                            <NavDropdown.Item onClick={() => setExpanded(false)} href="/corporate">Corporate</NavDropdown.Item>
+                            <NavDropdown.Item onClick={() => setExpanded(false)} href="/quin">The 'Quin</NavDropdown.Item>
+                            <NavDropdown.Item onClick={() => setExpanded(false)} href="/inhome">In Home</NavDropdown.Item>
                         </NavDropdown>
-                        <NavLink to="/staff" style={styles.link} activeStyle={styles.activeLink}>Staff</NavLink>
-                        <NavLink to="/training" style={styles.link} activeStyle={styles.activeLink}>Training</NavLink>
-                        <NavLink to="/contact" style={styles.link} activeStyle={styles.activeLink}>Contact</NavLink>
+                        <NavLink onClick={() => setExpanded(false)} to="/staff" style={styles.link} activeStyle={styles.activeLink}>Staff</NavLink>
+                        <NavLink onClick={() => setExpanded(false)} to="/training" style={styles.link} activeStyle={styles.activeLink}>Training</NavLink>
+                        <NavLink onClick={() => setExpanded(false)} to="/contact" style={styles.link} activeStyle={styles.activeLink}>Contact</NavLink>
                     </Nav>
                     <a href="https://www.instagram.com/optimalfitnessboston/?hl=en"><Instagram className="header-icon" size={24} style={{marginLeft: 8}}/></a>
                     <a href="https://www.facebook.com/dooitright/"><Facebook className="header-icon" size={24} style={{marginLeft: 8}} /></a>
